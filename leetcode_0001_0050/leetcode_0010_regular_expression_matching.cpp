0e7767f369733b2b4d569785147a0a2f294d2131
AlfengYuan <yyfgdut@gmail.com>
2022-05-01 14:42:20 +0800

    update question ten

--- leetcode_0001_0050/leetcode_0010_regular_expression_matching.cpp
+++ leetcode_0001_0050/leetcode_0010_regular_expression_matching.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
+using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        return MatchStr(s, p);
+        int n = s.length(), m = p.length();
+        s = ' ' + s, p = ' ' + p;
+        vector<vector<bool>> func(n+1, vector<bool>(m+1, false));
+        func[0][0] = true;
+        
+        for(int i = 0; i<=n; i++)
+        {
+            for(int j = 1; j<=m; j++)
+            {
+                if(j+1 <= m && p[j+1] == '*') continue;
+                if(p[j] != '*')
+                {
+                    func[i][j] = i && j && func[i-1][j-1] && (s[i] == p[j] || p[j] == '.');
+                }
+                else
+                {
+                    func[i][j] = (j >= 2 && func[i][j-2]) || (i && j && func[i-1][j] && (s[i]==p[j-1] || p[j-1] == '.'));
+                }
+            }
+        }
+        return  func[n][m];
     }
 };
 
