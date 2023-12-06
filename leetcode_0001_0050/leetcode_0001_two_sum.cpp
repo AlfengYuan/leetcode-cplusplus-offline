a93f8b19862a395872bf8755fc37ecc45a577923
AlfengYuan <2640706467@qq.com>
2023-12-06 19:15:23 +0800

    update

--- leetcode_0001_0050/leetcode_0001_two_sum.cpp
+++ leetcode_0001_0050/leetcode_0001_two_sum.cpp
@@ -1,36 +1,21 @@
-#include <iostream>
-#include <vector>
-using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        for (size_t i = 0; i < nums.size() - 1; i++)
+        vector<int> result;
+
+        for(int i = 0; i<nums.size()-1; i++)
         {
-            for (size_t j = i + 1; j < nums.size(); j++)
+            for(int j = i + 1; j < nums.size(); j++)
             {
-                if (nums[i] + nums[j] == target)
+                if(nums[i] + nums[j] == target)
                 {
-                    res.push_back(i);
-                    res.push_back(j);
-                    break;
+                    result.push_back(i);
+                    result.push_back(j);
                 }
             }
         }
-        return res;
+
+        return result;
+        
     }
 };
-
-int main()
-{
-    vector<int> nums = {2, 7, 11, 15};
-    Solution solution = Solution();
-    vector<int> res = solution.twoSum(nums, 9);
-    cout << "[";
-    for(size_t i = 0; i < res.size(); i++)
-    {
-        cout << res[i] << ", ";
-    }
-    cout << "]" << endl;
-    return 0;
-}
