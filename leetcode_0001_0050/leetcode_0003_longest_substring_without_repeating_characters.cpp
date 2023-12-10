7cd5df88bb6933d1bf41c5d6a94b518f330fe59f
AlfengYuan <2640706467@qq.com>
2023-12-10 23:55:39 +0800

    update

--- leetcode_0001_0050/leetcode_0003_longest_substring_without_repeating_characters.cpp
+++ leetcode_0001_0050/leetcode_0003_longest_substring_without_repeating_characters.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.length() <= 1) return s.length();
 
-        vector<int> func = {1};
-        for(int i = 1; i<s.length(); i++)
+        if (s.length() <= 1) return s.length();
+
+        int *fn = new int[s.length()];
+        int ascii[128] = {0};
+        fn[0] = 1;
+        int left, right;
+        for(right = 1; right < s.length(); right++)
         {
-            int j = 1;
-            for(;j <= func[i-1]; j++)
+            ascii[static_cast<int>(s[right])] += 1;
+            for(left = right - 1; (left >= 0 && ascii[static_cast<int>(s[left])] == 0); left--)
             {
-                if(s[i] == s[i-j]) break;
+                ascii[static_cast<int>(s[left])] += 1;
             }
-            func.push_back(j);
+
+            fn[right] = max({fn[right - 1], (right-left)});
+
+            for(int i = 0; i<128; i++)
+            {
+                ascii[i] = 0;
+            }
         }
-        return *max_element(func.begin(), func.end());
+
+        int result = fn[s.length() - 1];
+        delete []fn;
+        return result;
+      
+
     }
 };
-
-int main()
-{
-    string s = "abadfgghrhrtbrthhd";
-    Solution solution = Solution();
-    int res = solution.lengthOfLongestSubstring(s);
-    cout << res << endl; 
-    return 0;
-}
