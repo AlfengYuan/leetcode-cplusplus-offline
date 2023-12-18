d12a77257f218b39cc5d6d183976c831afe52aee
AlfengYuan <2640706467@qq.com>
2023-12-18 22:05:07 +0800

    update

--- leetcode_0001_0050/leetcode_0006_zigzag_conversion.cpp
+++ leetcode_0001_0050/leetcode_0006_zigzag_conversion.cpp
@@ -1,55 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if(numRows <= 1 || s.length() <= 1) return s;
-        vector<string> vecRes(numRows);
-        string res = "";
-        int row = -2;
-        bool dir = true;
-        for(int i = 0; i<s.length(); ++i)
+        if(numRows == 1) return s;
+        string result = "";
+        string strs[numRows];
+        int row = 0;
+        bool down = true;
+        for(char &c : s)
         {
-            if(dir == true && row < numRows-1)
+            if(row >=0 && row < numRows && down) strs[row++] += c;
+            else if(row == numRows)
             {
-               if(row <= -1) ++row;
-               vecRes[++row] += s[i];
+                down = false;
+                row -= 2;
+                strs[row--] += c; 
             }
-            else if(dir == false && row > -1)
+            else if(row >=0 && row < numRows && !down) strs[row--] += c;
+            else if(row < 0)
             {
-                if(row == numRows) --row;
-                vecRes[--row] += s[i]; 
+                down = true;
+                row += 2;
+                strs[row++] += c;
             }
-
-            if(dir == true && row == numRows-1)
-            {
-                dir = false;
-            }
-            else if(dir == false && row == 0)
-            {
-                dir = true;
-            }          
         }
 
         for(int i = 0; i<numRows; i++)
         {
-            res += vecRes[i];
+            result += strs[i];
         }
-        return res;
+
+        return result;
 
     }
 };
-
-int main(int argc, char *argv[])
-{
-    string test = "PAYPALISHIRING";
-    int numRows = 3;
-    Solution solution = Solution();
-    string res = solution.convert(test, 3);
-    std::cout << res << std::endl;
-    return 0;
-}
