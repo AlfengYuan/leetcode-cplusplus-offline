803644d364ee5ab819408c4c8ebe8209d9493b0b
AlfengYuan <2640706467@qq.com>
2023-12-18 22:06:55 +0800

    update

--- leetcode_0001_0050/leetcode_0005_longest_palindromic_substring.cpp
+++ leetcode_0001_0050/leetcode_0005_longest_palindromic_substring.cpp
@@ -1,56 +1,28 @@
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.length() <= 1) return s;
-        vector<vector<bool>> func(s.length(), vector<bool>(s.length(), false));
-        string res = "";
-
-        for(int i = s.length()-1; i>=0; i--)
+        for(int len = s.length(); len > 0; len--)
         {
-            for(int j = i; j<s.length() ; j++)
-            {
-                if(i == j) 
-                {
-                    func[i][j] = true;
-                }
-                else
-                {
-                    if(s[i] == s[j])
-                    {
-                        if(i+1 <= j-1)
-                        {
-                            func[i][j] = func[i+1][j-1];
-                        }
-                        else
-                        {
-                            func[i][j] = true;
-                        }
-                    }
-                    else
-                    {
-                        func[i][j] = false;
-                    }
-                }
-
-                if(func[i][j] && res.length() < j-i+1)
-                {
-                    res = s.substr(i, j-i+1);
-                }
+            for(int left = 0; left <= s.length() - len; left += 1)
+            {   
+                string str = s.substr(left, len);
+                if(IsPalindromic(str)) return str;
             }
         }
-        return res;
-    }
-};
 
-int main()
-{
-    string s = "cyyoacmjwjubfkzrrbvquqkwhsxvmytmjvbborrtoiyotobzjmohpadfrvmxuagbdczsjuekjrmcwyaovpiogspbslcppxojgbfxhtsxmecgqjfuvahzpgprscjwwutwoiksegfreortttdotgxbfkisyakejihfjnrdngkwjxeituomuhmeiesctywhryqtjimwjadhhymydlsmcpycfdzrjhstxddvoqprrjufvihjcsoseltpyuaywgiocfodtylluuikkqkbrdxgjhrqiselmwnpdzdmpsvbfimnoulayqgdiavdgeiilayrafxlgxxtoqskmtixhbyjikfmsmxwribfzeffccczwdwukubopsoxliagenzwkbiveiajfirzvngverrbcwqmryvckvhpiioccmaqoxgmbwenyeyhzhliusupmrgmrcvwmdnniipvztmtklihobbekkgeopgwipihadswbqhzyxqsdgekazdtnamwzbitwfwezhhqznipalmomanbyezapgpxtjhudlcsfqondoiojkqadacnhcgwkhaxmttfebqelkjfigglxjfqegxpcawhpihrxydprdgavxjygfhgpcylpvsfcizkfbqzdnmxdgsjcekvrhesykldgptbeasktkasyuevtxrcrxmiylrlclocldmiwhuizhuaiophykxskufgjbmcmzpogpmyerzovzhqusxzrjcwgsdpcienkizutedcwrmowwolekockvyukyvmeidhjvbkoortjbemevrsquwnjoaikhbkycvvcscyamffbjyvkqkyeavtlkxyrrnsmqohyyqxzgtjdavgwpsgpjhqzttukynonbnnkuqfxgaatpilrrxhcqhfyyextrvqzktcrtrsbimuokxqtsbfkrgoiznhiysfhzspkpvrhtewthpbafmzgchqpgfsuiddjkhnwchpleibavgmuivfiorpteflholmnxdwewj";
-    Solution solution = Solution();
-    string res = solution.longestPalindrome(s);
-    cout << res << endl;
-}
+        return "";
+
+    }
+
+private:
+    bool IsPalindromic(string &s)
+    {
+        for(int i = 0, j = s.length()-1; i<=j; i++,j--)
+        {
+            if(s[i] != s[j]) return false;
+        }
+
+        return true;
+    }
+
+};
