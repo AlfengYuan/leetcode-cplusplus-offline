9ea436e767c8f1cc69b403a4f152c00789a7c897
AlfengYuan <2640706467@qq.com>
2024-03-07 20:31:39 +0800

    update

--- leetcode_0001_0050/leetcode_0015_3sum.cpp
+++ leetcode_0001_0050/leetcode_0015_3sum.cpp
@@ -1,65 +1,31 @@
-#include <iostream>
-#include <algorithm>
-#include <stack>
-#include <vector>
-#include <set>
-
-using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> Vres;
-        if(nums.size() < 3) return Vres;
-        set<vector<int>> res;
-        vector<int> tmp(3, -1);
-        sort(nums.begin(), nums.end(),less<int>());
-        for(int i = 0; i<nums.size()-2; ++i )
-        {
-            for(int j = i+1, k = nums.size() -1; j < k;)
-            {
-                int curSum = nums[i] + nums[j] + nums[k];
-                if(curSum > 0)
-                {
-                    --k;
-                }
-                else if(curSum < 0)
-                {
-                    ++j;
-                }
-                else
-                {
-                    tmp[0] = nums[i];
-                    tmp[1] = nums[j];
-                    tmp[2] = nums[k];
-                    res.insert(tmp);
-                    --k;
-                    ++j;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for(int i = 0; i<nums.size()-2;i++){
+            if(i > 0 && nums[i]==nums[i-1]) continue;
+            if(nums[i] > 0) break;
+            int left = i+1, right = nums.size()-1;
+            int target = -nums[i];
+            while(left < right){
+                if(nums[left] + nums[right] == target){
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+                    left++;
+                    right--;
+                }else{
+                    if(nums[left] + nums[right] > target){
+                        right--;
+                    }else{
+                        left++;
+                    }
                 }
             }
         }
 
-        Vres.assign(res.begin(), res.end());
-
-        return Vres;
+        return res;
+        
     }
 };
-
-int main(int argc, char *argv[])
-{
-    vector<int> nums = {-1,0,1,2,-1,-4};
-    Solution solution = Solution();
-    vector<vector<int>> res = solution.threeSum(nums);
-    cout << "[";
-    for(vector<int> vecs : res)
-    {
-        cout << "[";
-        for(int vec: vecs)
-        {
-            cout << vec << ", ";
-        }
-        cout << "], ";
-    }
-    cout << "]" << endl;
-    
-    return 0;
-}
