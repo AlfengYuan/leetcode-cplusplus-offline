7aca3560cfa171d04c2b2fdef0a68203a06cf25f
AlfengYuan <2640706467@qq.com>
2024-03-09 13:44:08 +0800

    update

--- leetcode_0001_0050/leetcode_0003_longest_substring_without_repeating_characters.cpp
+++ leetcode_0001_0050/leetcode_0003_longest_substring_without_repeating_characters.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-
-        if (s.length() <= 1) return s.length();
-
-        int *fn = new int[s.length()];
-        int ascii[128] = {0};
-        fn[0] = 1;
-        int left, right;
-        for(right = 1; right < s.length(); right++)
-        {
-            ascii[static_cast<int>(s[right])] += 1;
-            for(left = right - 1; (left >= 0 && ascii[static_cast<int>(s[left])] == 0); left--)
-            {
-                ascii[static_cast<int>(s[left])] += 1;
-            }
-
-            fn[right] = max({fn[right - 1], (right-left)});
-
-            for(int i = 0; i<128; i++)
-            {
-                ascii[i] = 0;
+        for(int len = s.length(); len > 0; len--){
+            if(len > 128) continue;
+            for(int start = 0; start<=s.length() - len; start++){
+                string curr = s.substr(start, len);
+                if(check(curr)) return len;
             }
         }
 
-        int result = fn[s.length() - 1];
-        delete []fn;
-        return result;
-      
+        return 0;
 
+    }
+
+    bool check(string &s){
+        vector<int> vec(129, 0);
+        for(int i = 0; i<s.length(); i++){
+            if(vec[s[i]] > 0) return false;
+            vec[s[i]]++;
+        }
+        return true;
     }
 };
