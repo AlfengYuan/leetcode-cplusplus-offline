4749d35f812f0e5f6956a4b7da74a8871571540c
AlfengYuan <2640706467@qq.com>
2024-03-30 16:06:47 +0800

    update

---
+++ leetcode2/l00053.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        for(int i = 1; i<nums.size(); i++){
+            if(dp[i-1] > 0){
+                dp[i] = dp[i-1] + nums[i];
+            }else{
+                dp[i] = nums[i];
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
